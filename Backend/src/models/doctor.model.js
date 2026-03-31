d68cfc8914d21463b7bf6b23d4dcd33cf535d816
Prakash <prakasha.Guled@ametek.com>
2026-03-31 19:14:09 +0530

    mangose function corrections

--- Backend/src/models/doctor.model.js
+++ Backend/src/models/doctor.model.js
@@ -110,10 +110,9 @@ const doctorSchema = new Schema({
     }
 }, { timestamps: true })
 
-doctorSchema.pre("save", async function (next) {
+doctorSchema.pre("save", async function () {
     if (this.isModified("password"))
         this.password = await bcrypt.hash(this.password, 10)
-    next()
 })
 
 doctorSchema.methods.ispasswordcorrect = async function (password) {
--- Backend/src/models/patient.model.js
+++ Backend/src/models/patient.model.js
@@ -52,10 +52,9 @@ const patientSchema = new Schema({
 },{timestamps:true})
 
 
-patientSchema.pre("save", async function (next) {
+patientSchema.pre("save", async function () {
     if (this.isModified("password"))
         this.password = await bcrypt.hash(this.password, 10)
-    next()
 })
 
 patientSchema.methods.ispasswordcorrect = async function (password) {
